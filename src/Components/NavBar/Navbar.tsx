6d6d80a9c633338643c292373eff4eb5c7892003
Brendan Hurd <brendanhurd@gmail.com>
2023-08-22 14:05:45 +1000

    Add basic styles to navbar.

--- src/Components/NavBar/Navbar.tsx
+++ src/Components/NavBar/Navbar.tsx
@@ -1,12 +1,23 @@
 import { Link } from 'react-router-dom';
+import './navbar.scss';
 
 export const Navbar: React.FC = () => {
   return (
-    <div>
-      <Link to="/">Sad Bois</Link>
-      <Link to="/test">The Band</Link>
-      <Link to="/merchandise">Swag</Link>
-      <Link to="/login">Log In</Link>
-    </div>
+    <nav>
+      <ul>
+        <li className="home">
+          <Link to="/">Sad Bois</Link>
+        </li>
+        <li>
+          <Link to="/test">The Band</Link>
+        </li>
+        <li>
+          <Link to="/merchandise">Swag</Link>
+        </li>
+        <li>
+          <Link to="/login">Log In</Link>
+        </li>
+      </ul>
+    </nav>
   );
 };
