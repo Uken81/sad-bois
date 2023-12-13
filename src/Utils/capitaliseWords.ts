51f44709d493b6f48def8f136885994382894722
Brendan Hurd <brendanhurd@gmail.com>
2023-12-13 14:03:43 +1100

    Refactor the capitaliseWords function to to account for quotes.

--- src/Utils/capitaliseWords.ts
+++ src/Utils/capitaliseWords.ts
@@ -7,6 +7,20 @@ export const capitaliseWords = (str: string) => {
   return str
     .toLowerCase()
     .split(' ')
-    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .map((word) => {
+      // Capitalize the first character
+      let capitalized = word.charAt(0).toUpperCase() + word.slice(1);
+
+      // Find and capitalize character following a single or double quote
+      const quoteIndex = word.indexOf("'") >= 0 ? word.indexOf("'") : word.indexOf('"');
+      if (quoteIndex >= 0 && quoteIndex + 1 < word.length) {
+        capitalized =
+          capitalized.substring(0, quoteIndex + 1) +
+          capitalized.charAt(quoteIndex + 1).toUpperCase() +
+          capitalized.substring(quoteIndex + 2);
+      }
+
+      return capitalized;
+    })
     .join(' ');
 };
