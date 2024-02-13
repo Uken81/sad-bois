c48608fda5d2aa7ffadc0db6b2af9cbd7d0e9838
Brendan Hurd <brendanhurd@gmail.com>
2024-02-13 16:31:42 +1100

    Images

--- src/Routes/HomePage/Banner.tsx
+++ src/Routes/HomePage/Banner.tsx
@@ -1,7 +1,12 @@
+import bannerImage from './banner1.png';
 export const Banner: React.FC = () => {
   return (
     <div className="banner">
-      <div className="h-[32rem] w-screen bg-[url('../../../public/Assets/Banners/banner1.png')] bg-cover bg-center" />
+      <div
+        className="h-[32rem] w-screen bg-cover bg-center"
+        style={{ backgroundImage: `url(${bannerImage})` }}
+      />
+      {/* <div className="h-[32rem] w-screen bg-[url('./banner1.png')] bg-cover bg-center" /> */}
     </div>
   );
 };
