0c60d84fc72e1c694bc725aca7019e17c40cf77f
Brendan Hurd <brendanhurd@gmail.com>
2024-02-14 10:37:59 +1100

    test

--- src/Routes/HomePage/Banner.tsx
+++ src/Routes/HomePage/Banner.tsx
@@ -1,7 +1,7 @@
 export const Banner: React.FC = () => {
   return (
     <div className="banner">
-      <div className="h-[32rem] w-screen bg-[url('../../../public/Assets/Banners/banner1.png')] bg-cover bg-center" />
+      <div className="h-[32rem] w-screen bg-[url('/Assets/Banners/banner1.png')] bg-cover bg-center" />
     </div>
   );
 };
