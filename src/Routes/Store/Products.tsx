8cdcd1b17a7782eaa4f5b584101c893e3a902625
Brendan Hurd <brendanhurd@gmail.com>
2024-02-15 10:57:55 +1100

    Add loading text when user selects new category in store but new products list has not rendered.

--- src/Routes/Store/Products.tsx
+++ src/Routes/Store/Products.tsx
@@ -1,12 +1,23 @@
 import { ProductType } from '../../DataLoaders/productsLoader';
 import { ProductCard } from './ProductCard/ProductCard';
 
-export const Products: React.FC<{ regularProducts: ProductType[] }> = ({ regularProducts }) => {
+export const Products: React.FC<{ regularProducts: ProductType[]; loadingCategory: boolean }> = ({
+  regularProducts,
+  loadingCategory
+}) => {
   return (
-    <div className=" my-14 grid grid-cols-1 justify-items-center gap-6 md:mx-16 md:grid-cols-2 md:gap-6 lg:grid-cols-3 lg:gap-10 xl:mx-40 xl:gap-12">
-      {regularProducts.map((item) => {
-        return <ProductCard key={item.id} product={item} />;
-      })}
-    </div>
+    <>
+      {loadingCategory ? (
+        <div className="my-10 text-center text-4xl">
+          <p>Loading.....</p>
+        </div>
+      ) : (
+        <div className="my-14 grid grid-cols-1 justify-items-center gap-6 md:mx-16 md:grid-cols-2 md:gap-6 lg:grid-cols-3 lg:gap-10 xl:mx-40 xl:gap-12">
+          {regularProducts.map((item) => (
+            <ProductCard key={item.id} product={item} />
+          ))}
+        </div>
+      )}
+    </>
   );
 };
--- src/Routes/Store/Store.tsx
+++ src/Routes/Store/Store.tsx
@@ -4,10 +4,12 @@ import { FeaturedCarousel } from './Featured/FeaturedCarousel';
 import { Products } from './Products';
 import { NoData } from '../../Components/NoData';
 import { StoreCategoryContextType } from '../RouteWrappers/storeWrapper';
+import { useEffect, useState } from 'react';
 
 export const Store: React.FC = () => {
   const loaderData = useLoaderData() as MerchandiseType;
   const { selectedCategory } = useOutletContext() as StoreCategoryContextType;
+  const [loadingCategory, setloadingCategory] = useState(false);
   const featuredProducts = loaderData.camelisedFeaturedProducts?.length
     ? loaderData.camelisedFeaturedProducts
     : null;
@@ -17,12 +19,21 @@ export const Store: React.FC = () => {
 
   const displayFeatured = featuredProducts && featuredProducts.length && selectedCategory === 'all';
 
+  //TODO: Find better way to handle category change or loading status than below side effects. Consider filtering returned products on Frontend again.
+  useEffect(() => {
+    setloadingCategory(true);
+  }, [selectedCategory]);
+
+  useEffect(() => {
+    setloadingCategory(false);
+  }, [regularProducts]);
+
   return (
     <main>
       <h1 className="text-center text-h1 font-h1 md:text-6xl">Sad Boi Swag</h1>
       {displayFeatured ? <FeaturedCarousel featuredProducts={featuredProducts} /> : null}
       {regularProducts ? (
-        <Products regularProducts={regularProducts} />
+        <Products regularProducts={regularProducts} loadingCategory={loadingCategory} />
       ) : (
         <NoData title="Could Not Find Products" />
       )}
