d80255486d8bba12fe67d5cf17da246682cbba92
Brendan Hurd <brendanhurd@gmail.com>
2024-02-15 14:36:52 +1100

    Clean up redundant console logs in currency formatter.

--- src/Utils/currencyFormatter.ts
+++ src/Utils/currencyFormatter.ts
@@ -13,7 +13,6 @@ const currencyFormatter = new Intl.NumberFormat('en-US', {
 
 export const formatCurrency = (value: number | null) => {
   if (!value) {
-    console.log('errorVal', value);
     console.error('invalid value');
     return;
   }
