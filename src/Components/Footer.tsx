e01c4da72291bc41d7c61e5c2e2b6ff1acf670d4
Brendan Hurd <brendanhurd@gmail.com>
2024-03-21 12:35:09 +1100

    Fix footer styles on small screens.

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -3,20 +3,18 @@ import { CgCopyright } from 'react-icons/cg';
 
 export const Footer: React.FC = () => {
   return (
-    <footer className="mt-auto">
-      <ul className="flex flex-row items-center justify-center bg-secondary text-sm text-white ">
+    <footer className="mt-auto text-center text-xs text-white md:text-lg">
+      <ul className="flex flex-row items-center justify-center space-x-2 bg-secondary">
         <li className="py-2 text-sm">
           <HomeLink textSize="sm" />
         </li>
 
         <li className=" flex flex-row ">
           <CgCopyright />
-          <span className="border-r-2 border-white pr-2">
-            2024 The Sad Bois. Melancholie Music Inc.
-          </span>
+          <span>2024 The Sad Bois. Melancholie Music Inc.</span>
         </li>
         <li>
-          <span className="pl-2">All rights reserved</span>
+          <span>All rights reserved</span>
         </li>
       </ul>
     </footer>
