13333f15a93d111ab8e10e2f82469120695f75d5
Brendan Hurd <brendanhurd@gmail.com>
2024-04-10 17:24:49 +1000

    Create LogoutErrorModal componant and move its state to UserOptions closer to its consumer

--- src/Components/NavBar/Menu/UserOptions.tsx
+++ src/Components/NavBar/Menu/UserOptions.tsx
@@ -2,19 +2,14 @@ import { useState } from 'react';
 import { Logout } from '../Links/Logout';
 import { Profile } from '../Links/Profile';
 import { UserDropdown } from './UserDropdown';
-import { Modal } from '../../Modal';
+import { LogoutErrorModal } from './LogoutErrorModal';
 
 export const UserOptions: React.FC = () => {
   const [showModal, setShowModal] = useState(false);
 
   return (
     <>
-      <Modal id="logout" isOpen={showModal} setShowModal={setShowModal}>
-        <div className="text-center">
-          <p className="text-lg font-bold">Network Error</p>
-          <p className="font-bold">Failed to successfully log out.</p>
-        </div>
-      </Modal>
+      <LogoutErrorModal showModal={showModal} setShowModal={setShowModal} />
       <div className="mb-2 border-l-2 border-secondary pl-1 duration-500 hover:border-accent lg:hidden">
         <Profile />
       </div>
